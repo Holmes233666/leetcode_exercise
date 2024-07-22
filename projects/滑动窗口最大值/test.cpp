3128ab758ec8c5a905e8f21993eeb13bf6dae73d
Holmes233666 <2232146110@qq.com>
2024-07-22 15:58:56 +0800

    0722

--- projects/滑动窗口最大值/test.cpp
+++ projects/滑动窗口最大值/test.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<queue>
 #include<unordered_map>
+#include<algorithm>
 
 using namespace std;
 
@@ -9,8 +10,35 @@ using namespace std;
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
+        vector<int> res;
+        // 主要时间花费在找这个区间内的最大的元素是什么
+        for (int left = 0, right = k - 1; right < nums.size(); right++, left++) {
+            auto maxNum = max_element(nums.begin()+left, nums.begin()+right+1);
+            res.push_back(*maxNum);
+        }
+        return res;
+    }
+};
 
+// 大顶堆：关键在于不急于移出离开滑动窗口的元素，延迟移出，直到这个元素到堆顶，移除的时间复杂度由O(n)降低到O(1)
+class Solution2 {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> queue; 
+        for (int i = 0; i < k; i++) {
+            queue.emplace(nums[i], i);
+        }
+        vector<int> res;
+        res.push_back(queue.top().first);
+        for (int left = 1, right = k; left < nums.size(), right < nums.size(); left++, right++) {
+            queue.emplace(nums[right], right);
+            while (queue.top().second < left) {
+                // 弹出堆顶元素
+                queue.pop();
+            }
+            res.push_back(queue.top().first);
+        }
+        return res;
     }
 };
 
