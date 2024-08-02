dd4bab5d2f9016de7583b4a506dfa4880b6aa237
Holmes233666 <2232146110@qq.com>
2024-08-02 23:51:32 +0800

    0802

--- projects/两两交换链表中的结点/test.cpp
+++ projects/两两交换链表中的结点/test.cpp
@@ -1,1 +1,26 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode *pre = new ListNode();
+        ListNode *res = head->next;
+        ListNode *p = head, *q = head->next;
+        while (p!=nullptr && q!= nullptr) {
+            p->next = q->next;
+            pre->next = q;
+            q->next = p;
+            pre = p;
+            p = p->next;
+            q = q->next;
+        }
+        return res;
+    }
+};
